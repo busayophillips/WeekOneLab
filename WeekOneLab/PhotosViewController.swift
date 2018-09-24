a105a5c6fc1205c2f3f1502746687f4cd4aa6818
Busayo Phillips <Busayo@Busayo-Phillipss-MacBook-Pro.local>
2018-09-24 16:16:22 -0400

    Ready

--- WeekOneLab/PhotosViewController.swift
+++ WeekOneLab/PhotosViewController.swift
@@ -1,8 +1,8 @@
 //
 //  PhotosViewController.swift
-//  Tumblr
+//  WeekOneLab
 //
-//  Created by Busayo Phillips on 9/23/18.
+//  Created by Busayo Phillips on 9/24/18.
 //  Copyright © 2018 Busayo Phillips. All rights reserved.
 //
 
@@ -23,7 +23,6 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.rowHeight = 200
         
-        // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
@@ -33,14 +32,12 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             } else if let data = data,
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 print(dataDictionary)
-                
-                // TODO: Get the posts and store in posts property
-                // Get the dictionary from the response key
+
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
-                // Store the returned array of dictionaries in our posts property
+               
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 
-                // TODO: Reload the table view
+    
                 self.tableView.reloadData()
             }
         }
@@ -57,23 +54,19 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         
         let post = posts[indexPath.row]
         if let photos = post["photos"] as? [[String: Any]] {
-            // photos is NOT nil, we can use it!
-            // TODO: Get the photo url
-            // 1.
+   
             let photo = photos[0]
-            // 2.
+
             let originalSize = photo["original_size"] as! [String: Any]
-            // 3.
+
             let urlString = originalSize["url"] as! String
-            // 4.
+
             let url = URL(string: urlString)
             cell.photoImageView.af_setImage(withURL: url!)
             
             
             
         }
-        // Configure YourCustomCell using the outlets that you've defined.
-        
         return cell
     }
     
@@ -100,18 +93,11 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             
             vc.url = url
             tableView.deselectRow(at: indexPath, animated: true)
-            // photos.photoView.af_setImage(withURL: url!)
-            
-            
         }
         
-        
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 }
 
